494a55c392c69073ed02d783358f0b48d25905bd
lenovo <1549002133@qq.com>
2017-12-28 17:55:13 +0800

    特征值

--- okex/OkexClient.py
+++ okex/OkexClient.py
@@ -6,7 +6,6 @@ import json
 import numpy as np
 import pandas as pd
 
-
 # 初始化apikey，secretkey,url
 apikey = 'XXXX'
 secretkey = 'XXXXX'
@@ -21,7 +20,6 @@ print('K线数据')
 # 方式2 使用本地保存的数据(不是最新)，避免接口连接缓慢或超时
 kline_data_json = json.loads(open('kline_data.txt', 'r').read())
 
-
 kline_data = pd.DataFrame(kline_data_json,
                           columns=['time', 'open', 'high', 'low', 'close', 'volume'])
 print(kline_data)
@@ -32,16 +30,16 @@ high_prices = np.array(kline_data['high'], dtype=np.double)
 low_prices = np.array(kline_data['low'], dtype=np.double)
 volumes = np.array(kline_data['volume'], dtype=np.double)
 
-#通过数据计算指标
-ma7_data = talib.MA(close_prices,timeperiod=7)
-ma30_data = talib.MA(close_prices,timeperiod=30)
+# 通过数据计算指标
+ma7_data = talib.MA(close_prices, timeperiod=7)
+ma30_data = talib.MA(close_prices, timeperiod=30)
 # wma_data = talib.WMA(close_prices)
 # mom_data = talib.MOM(close_prices)
 # stck, stcd = talib.STOCH(high_prices, low_prices, close_prices)
 macd, macdsignal, macdhist = talib.MACD(close_prices)
-rsi_data = talib.RSI(close_prices,timeperiod=10)
-boll_upper, boll_middle, boll_lower= talib.BBANDS(close_prices)
-print('macd',macd)
+rsi_data = talib.RSI(close_prices, timeperiod=10)
+boll_upper, boll_middle, boll_lower = talib.BBANDS(close_prices)
+print('macd', macd)
 
 # 保存训练数据中的一组训练数据
 features = []
@@ -53,3 +51,4 @@ features.append(rsi_data)
 features.append(boll_upper)
 features.append(boll_middle)
 features.append(boll_lower)
+print(features)
